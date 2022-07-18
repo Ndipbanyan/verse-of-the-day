a9ceeb30ad75c590e0b13796750ec52ff4be1737
Ndipbanyan <agnesjedidiah09@gmail.com>
2022-07-18 15:46:04 +0100

    Scrape youversion for verse of the day

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
+
+const url = 'https://www.bible.com/verse-of-the-day'
+
+app.get('/', function (req, res) {
+	res.json('This is my webscraper')
+})
+
+app.get('/results', (req, res) => {
+	axios(url)
+		.then((response) => {
+			const html = response.data
+			const $ = cheerio.load(html)
+			const articles = []
+
+			$('.usfm ', html).each(function () {
+				const title = $(this).text()
+				articles.push(title)
+			})
+			res.json(articles[0])
+		})
+		.catch((err) => console.log(err))
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
