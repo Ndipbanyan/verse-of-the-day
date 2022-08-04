06d5f7e7b8d7c621c2e1e861c508dce439e0bc69
Ndipbanyan <agnesjedidiah09@gmail.com>
2022-08-04 04:40:00 +0100

    call api to get verse if not today's date

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,42 @@
 const verseElement = document.getElementById('verse')
 const prayerElement = document.getElementById('prayer')
+const scriptureTextElement = document.getElementById('scripture-text')
 let verse
-let versions
-const BIBLE_ACCESS_KEY = ''
 
-fetch('http://localhost:8000/results')
-	.then((res) => res.json())
-	.then((data) => {
-		verseElement.textContent = data
-		const formatData = data.split('(')
-		verse = formatData[0].trim()
+let bibleVerse
+const reqBaseUrl = 'http://localhost:8080/'
+const fetchVerse = fetch(`${reqBaseUrl}verse`).then((res) => res.json())
+const fetchVersions = fetch(`${reqBaseUrl}scripture-text`).then((res) => res.json())
 
-		verseElement.innerText = verse
-	})
-	.catch((err) => console.log(err))
+const dataFromLS = localStorage.getItem('versions')
+
+if (dataFromLS) {
+	const verseFromLS = JSON.parse(dataFromLS)
+
+	let today = new Date().toLocaleString('en-GB', { timeZone: 'UTC' })
+	today = today.split(',')
+	today = today[0]
+
+	if (today == verseFromLS.timestamp) {
+		verseElement.textContent = verseFromLS.data[0]
+		scriptureTextElement.textContent = verseFromLS.data[1].KJV
+	} else {
+		Promise.all([fetchVerse, fetchVersions])
+
+			.then(function (data) {
+				verseElement.textContent = data[0]
+				scriptureTextElement.textContent = data[1].KJV
+				let today = new Date().toLocaleString('en-GB', { timeZone: 'UTC' })
+				today = today.split(',')
+				today = today[0]
+				const savetoLS = { data, timestamp: today }
+				localStorage.setItem('versions', JSON.stringify(savetoLS))
+			})
+			.catch(function (error) {
+				console.log(error)
+			})
+	}
+}
 
 // get background Image
 function setImage(image) {
@@ -26,9 +49,9 @@ document.addEventListener('DOMContentLoaded', () => {
 		if (data.background) {
 			setImage(data.background)
 			console.log('data', data)
-			console.log({ datestored: data.timestamp, datetoday: today })
+			console.log({ datestored: data.timestamp, datetoday: today, bible: bibleVerse })
 		}
-		if (data.timestamp) console.log(data.timestamp)
+		// if (data.timestamp) console.log(data.timestamp)
 	})
 
 	chrome.runtime.sendMessage({ command: 'next-image' })
